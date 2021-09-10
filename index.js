d2566bba322849fd8a9274c2ea3c140819c07eb0
Vichithrakumar <41378826+Vichithrakumar@users.noreply.github.com>
2021-09-10 09:04:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! This is Vichithrakumar Testing\n'
+  const msg = 'Vichithrakumar Testing\n'
   res.end(msg);
 });
 
